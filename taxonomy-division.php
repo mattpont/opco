48f0c2f06bc2eea8da99b74bb184de2834dd5b72
Matthew Pont <matthew.pont@gmail.com>
2025-01-22 14:05:46 +0000

    Update taxonomy-division.php

--- taxonomy-division.php
+++ taxonomy-division.php
@@ -41,7 +41,8 @@ if ( get_query_var('region') ) {
 			</div>
 			<div class="row">
 				<div class="col text-center">
-					<h2 class="h3 mt-0 mb-5">Your results:</h1>
+					<h2 class="h3 mt-0 mb-5">Your results:</h2>
+					<?=get_field("your_results_text")?>
 				</div>
 			</div>
 
