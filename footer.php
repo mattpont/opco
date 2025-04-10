da4b0ad3e09ff0eba18c46bc3dc964071a995361
Matthew Pont <matthew.pont@gmail.com>
2025-04-10 10:31:42 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
 <?php
-if ( !is_page(array(57,61,65)) ) {
+if ( !is_page(array(57,61,65,67)) ) {
 ?>
 <div id="footer-top"></div>
 <div class="wrapper mt-5 fixed-bottom" id="wrapper-footer">
